51c479b917cd7b2d5eda0676478dc48c979f150d
DanielekPark <54155014+DanielekPark@users.noreply.github.com>
2025-10-04 18:54:42 -0700

    removed council-members.js files

--- src/api/council-member/routes/council-member.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * council-member router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::council-member.council-member');
--- src/api/council-member/services/council-member.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * council-member service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::council-member.council-member');
